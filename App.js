6c7682ddb7ff7b415cc56cc09c64f43053c441f4
Lingling <Liliwang59220@gmail.com>
2021-04-21 21:36:33 -0700

    writing

--- App.js
+++ App.js
@@ -1,16 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is hard</Text>
+      <Text>oprn rcpo.jpn something</Text>
       <StatusBar style="auto" />
+      <Text>Hello Anyone</Text>
+      <Image
+        style={styles.profilePic}
+        sources={{uri: 'https://www.eachoneteachone.is/uploads/4/6/3/3/46337981/editor/img-5449.jpg?1611880626'}}
+      />
+      <Text style={styles.name}>LingLing Wang</Text>
+      <Text style={styles.email}>Liliwang59220@gmail.com</Text>
+      <TouchableOpacity
+        style={styles.button}></TouchableOpacity>
 
+        <Text style={styles.buttonText}>My website</Text>
+        <Text style={styles.question}>Why did you want to learn mobile app development?</Text>
+        <Text style={styles.answer}>I wanted to learn how to make apps because I have a cellphone and we use apps for almost everything. I thought it would be cool to learn how to make an app and download it on my own device.
+        </Text>
       </View>
+
   );
-}
+};
+
+const styles = StyleSheet.create({
+  baseText: {
+    fontFamily: "Cochin"
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
+export default TextInANest;
+const styles = StyleSheet.create({
+  baseText: {
+    fontWeight: 'bold',
+  },
+  innerText: {
+    color: 'red',
+  },
+});
 
 const styles = StyleSheet.create({
   container: {
